103a8bc8c6c9095fa3a589d5051fa11be80c80b2
Hammad_DevX <iamhammaddev03@gmail.com>
2025-09-09 04:00:37 +0500

    Error Resolved

--- Face & Object Detection/face_eye_smile.py
+++ Face & Object Detection/face_eye_smile.py
@@ -16,22 +16,21 @@ while True:
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
 
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = frame[y:y+h, x:x+w]
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
 
-    eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 10)
-    if len(eyes) > 0:
-        cv2.putText(frame, "Eyes Detected", (x, y-30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 10)
+        if len(eyes) > 0:
+            cv2.putText(frame, "Eyes Detected", (x, y-30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
 
-    smiles = smile_cascade.detectMultiScale(roi_gray, 1.7, 20)
-    if len(smiles) > 0:
-        cv2.putText(frame, "Smiling...", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+        smiles = smile_cascade.detectMultiScale(roi_gray, 1.7, 20)
+        if len(smiles) > 0:
+            cv2.putText(frame, "Smiling...", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
 
     cv2.imshow("Smart Face Detector", frame)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
-    
 cap.release()
 cv2.destroyAllWindows()
